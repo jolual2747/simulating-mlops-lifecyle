587880068b72b0809b2dd8bfbc75e5a33fb62b7c
Jose Alcocer <jolual2747@gmail.com>
2023-04-25 23:16:56 -0500

    training module

--- training.py
+++ training.py
@@ -5,10 +5,13 @@ from sklearn.cluster import SpectralClustering, KMeans
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score
-from sklearn.preprocessing import OrdinalEncoder
+from sklearn.metrics import accuracy_score, silhouette_score
+from sklearn.preprocessing import OrdinalEncoder, MinMaxScaler
 from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.svm import SVC
+from sklearn.pipeline import Pipeline
 import warnings
+
 
 warnings.filterwarnings('ignore')
 
@@ -35,10 +38,11 @@ def create_clusters(df):
     num_df = pd.DataFrame(scaler.fit_transform(nums_df), columns=num_cols)
     cat_df = pd.get_dummies(cats_df, dtype= 'float')
     aux_df = pd.concat([num_df, cat_df], axis = 1)
-    spectral = SpectralClustering(n_clusters=2, n_jobs=-1, affinity= 'nearest_neighbors', n_neighbors=7, random_state = 1234, assign_labels = 'kmeans')
+    spectral = SpectralClustering(n_clusters=2, n_jobs=-1, affinity = 'sigmoid', n_neighbors=5, random_state = 1234, assign_labels = 'kmeans')
     df['label'] = spectral.fit_predict(aux_df)   
     df.to_csv('datalake/customers_clustered.csv', index=False)
     print('Dataset with clusters succesfully saved to Datalake!')
+    print(f"silhouette_score: {silhouette_score(aux_df, df['label'].values)}")
 
 def feature_engineering(df):
     print('Initiliazing feature engineering...')
@@ -53,18 +57,21 @@ def feature_engineering(df):
     encoded = encoder.fit_transform(df[cat_cols])
     chi2_test = SelectKBest(chi2, k = 3).set_output(transform='pandas')
     best_cat_features = chi2_test.fit_transform(encoded, df['label'])
-    aux = pd.concat([best_num_features, best_cat_features], axis = 1)
-    return pd.get_dummies(aux, dtype='float')
+    aux = pd.concat([best_num_features, df[best_cat_features.columns.tolist()]], axis = 1)
+    return pd.get_dummies(aux, dtype='float64')
 
 def train_model(df):
-    features = feature_engineering(df)
-    scaler = MinMaxScaler()
+    num_cols, cat_cols = num_and_cat_cols(df)
+    scaler = MinMaxScaler().set_output(transform='pandas')
+    scaled = scaler.fit_transform(df[num_cols])
+    new_df = pd.concat([scaled, df[cat_cols]], axis=1)
+    features = feature_engineering(new_df)
     print('Initializing training...')
     X, y = features.drop(columns=['label']), features['label'].values
     X_train, X_test, y_train, y_test = train_test_split(X, y)
     tree = DecisionTreeClassifier()
-    param_grid = {'max_depth': [3, 5, 7, 10], 'max_leaf_nodes': [3, 5, 7, 10], 'min_samples_split': [2,3,6]}
-    grid = GridSearchCV(tree, param_grid=param_grid, n_jobs=-1, cv = 5, verbose=0, scoring='roc_auc')
+    param_grid = {'max_depth': [20, 25]}
+    grid = GridSearchCV(tree, param_grid=param_grid, n_jobs=-1, cv = 5, verbose=0, scoring='accuracy')
     grid.fit(X_train, y_train)
     model = grid.best_estimator_
     print(f"Params of best model: {grid.best_params_} ")
